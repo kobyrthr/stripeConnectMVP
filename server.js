64e6ae45fd7094dad557459f51ab37e0df4a5f1f
kobyrthr-dev <kobyrthr@gmail.com>
2023-02-17 22:57:52 -0500

    README update

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const PORT = 3000
 
 // Set your secret key. Remember to switch to your live secret key in production.
 // See your keys here: https://dashboard.stripe.com/apikeys
-const stripe = require('stripe')('sk_test_51EbFR1LtEmbOulQpytSyee4NmDKdaNozYDu99asRDs1l4AWRQxrBQ9efFWfH8lS31Hg0ZTk3TP2My1cDVy3FZXW100TJZ0vn8B');
+const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const account = async ()=>{
      data = await stripe.accounts.create({type: 'express'});
